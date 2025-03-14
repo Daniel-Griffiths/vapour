c9e3265720d00c9e860ce4c9cf05fcd4edbe10ce
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2025-03-14 00:11:49 +0000

    add basic gamepad support

--- README.md
+++ README.md
@@ -25,14 +25,35 @@ And to create a release build run the following command:
 
 ```bash
 # Windows
-yarn build --win
+yarn build:windows
 
 # Mac
-yarn build --mac
+yarn build:mac
 
 # Linux
-yarn build --linux
+yarn build:linux
+```
 
+## For linux flatpak
+
+Unfortunately electron-builder does not yet competently build flatpaks, and the older approach using electron-packager and electron-installer-flatpak can't handle native modules. A work-around is to use electron-builder for the packaging step and electron-installer-flatpak for the actual flatpak creation.
+
+First you need to run `yarn install -g @malept/electron-installer-flatpak` (this can't be added as dev-dependency since it is not cross-platform and yarn doesn't allow optional dev-dependencies).
+
+Then
+
+```bash
+yarn run build:dist
+yarn run build:linuxdir
+yarn run build:flatpak
+```
+
+In order for this to work you must have already installed flatpak-builder using your favorite package manager (e.g. sudo pamac install flatpak-builder) and run:
+
+```
+flatpak install flathub org.freedesktop.Platform//19.08;
+flatpak install org.freedesktop.Sdk//19.08;
+flatpak install org.electronjs.Electron2.BaseApp/x86_64/stable
 ```
 
 ## Running Github Actions Locally
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
+// @ts-ignore - TODO: gamepad.js is not typed
+import { GamepadListener } from "gamepad.js";
 import { HashRouter } from "react-router-dom";
 import { init } from "@noriginmedia/norigin-spatial-navigation";
 
@@ -11,7 +13,70 @@ init({
   visualDebug: false,
 });
 
+// Define key mappings outside component - only created once
+const KEY_MAPPINGS: Record<string, { key: string; keyCode: number }> = {
+  enter: { key: "Enter", keyCode: 13 },
+  esc: { key: "Escape", keyCode: 27 },
+  up: { key: "ArrowUp", keyCode: 38 },
+  left: { key: "ArrowLeft", keyCode: 37 },
+  down: { key: "ArrowDown", keyCode: 40 },
+  right: { key: "ArrowRight", keyCode: 39 },
+};
+
 export function App() {
+  const pressKey = (key: string) => {
+    const keyProps = KEY_MAPPINGS[key] || {
+      key,
+      keyCode: key.charCodeAt(0),
+    };
+
+    const keyEvent = new KeyboardEvent("keydown", {
+      key: keyProps.key,
+      code: keyProps.key,
+      keyCode: keyProps.keyCode,
+      which: keyProps.keyCode,
+      bubbles: true,
+      cancelable: true,
+    });
+
+    document.dispatchEvent(keyEvent);
+  };
+
+  /**
+   * Listen for gamepad events and dispatch key presses
+   */
+  useEffect(() => {
+    const listener = new GamepadListener();
+
+    // TODO: gamepad.js is not typed
+    listener.on("gamepad:button", (event: any) => {
+      const { button, pressed } = event.detail;
+
+      if (!pressed) return;
+
+      switch (button) {
+        case 12:
+          return pressKey("up");
+        case 13:
+          return pressKey("down");
+        case 14:
+          return pressKey("left");
+        case 15:
+          return pressKey("right");
+        case 0:
+          return pressKey("enter");
+        case 1:
+          return pressKey("esc");
+      }
+    });
+
+    listener.start();
+
+    return () => {
+      listener.stop();
+    };
+  }, []);
+
   return (
     <React.StrictMode>
       <HashRouter>
